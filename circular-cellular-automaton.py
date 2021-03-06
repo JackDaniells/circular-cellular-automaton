bd1757d5c11e8771649e3d193f704366468669c4
Daniel Kock <daniel.kock@mercadolivre.com>
2021-03-06 13:47:46 -0300

    add cell.in & cell.out files adn adapt code to use them

--- circular-cellular-automaton.py
+++ circular-cellular-automaton.py
@@ -1,5 +1,5 @@
-import copy
 
+# -------------------------------------- #
 # Classe que representa uma célula do autômato celular
 class Cell:
     def __init__(self, position, value):
@@ -9,9 +9,7 @@ class Cell:
     def __str__(self):
         return "position=" + str(self.position) + "value=" + str(self.value)
 
-# n = size  
-# m = limit
-# d = distance
+# -------------------------------------- #
 # Classe que representa o autômato celular
 class Automaton:
     def __init__(self, size, distance, limit, initialValues):
@@ -62,15 +60,12 @@ class Automaton:
         self.grid = newGrid
 
     def __str__(self):
-        strAutomaton = "[ "
+        strAutomaton = ""
         for cell in self.grid:
             strAutomaton += str(cell.value) + " "
-        strAutomaton += "]"
         return strAutomaton
 
-
-
-# k = executions
+# -------------------------------------- #
 # classe que representa o motor que executa as etapas do autômato
 class Engine:
     def __init__(self, executions, automaton):
@@ -80,21 +75,92 @@ class Engine:
     def run(self):
         for i in range(self.executions):
             self.automaton.nextStep()
-            # print("Step " + str(i + 1) + ": " + str(self.automaton))
 
 
+# -------------------------------------- #
+# classe responsável por ler o arquivo de entrada e escrever a saída
+class Reader:
+    def __init__(self, inFile, outFile):
+        self.inFile = inFile
+        self.outFile = outFile
+
+    # método responsável por validar a entrada
+    def validate(self, args, values):
+        n = args[0]
+        m = args[1]
+        d = args[2]
+        k = args[3]
+        
+        hasError = False
+        if n < 1 or n > 500:
+            print("n fora do intervalo estabelecido (1 ≤ n ≤ 500)")
+            hasError = True
+
+        if m < 1 or m > 1000000:
+            print("m fora do intervalo estabelecido (1 ≤ m ≤ 1000000)")
+            hasError = True
+
+        if d >= n/2:
+            print("d fora do intervalo estabelecido (d < n/2)")
+            hasError = True
+
+        if k < 1 or m > 10000000:
+            print("k fora do intervalo estabelecido (1 ≤ k ≤ 10000000)")
+            hasError = True
+
+        validValues = all(v <= m for v in values)
+        if not validValues:
+            print("valores iniciais das células do autômato fora do intervalo estabelecido (1 ≤ m ≤ 1000000)")
+            hasError = True
+
+        if hasError:
+            exit()
+    
+        return n, m, d, k, values
+
+
+    # método responsável por fazer a leitura do arquivo de entrada
+    def readInput(self):
+        try:
+            f = open(self.inFile, "r")
+            cellIn = f.read()
+            cellSplit = cellIn.splitlines()
+            args = cellSplit[0].split(sep=" ")
+            values = cellSplit[1].split(sep=" ")
+
+            args = [ int(i) for i in args ]
+            if len(args) != 4:
+                raise Exception()
+
+            values = [ int(i) for i in values ]
+            return self.validate(args, values)
+        except:
+            print("Erro na leitura do arquivo " + self.inFile + ", revise o formato da entrada")
+            exit()
+        finally:
+            f.close()
+
+    # método responsável por escrever o resultado da execução no arquivo de saída
+    def writeOutput(self, output):
+        f = open(self.outFile, "w")
+        f.write(output)
+        f.close()
+
+
+# -------------------------------------- #
 def main():
-    n = 5
-    m = 3
-    d = 1
-    k = 10
-    i = [1, 2, 2, 1, 2]
-
+    # instancia o objeto responsavel pela leitura dos arquivos
+    reader = Reader(inFile="cell.in", outFile="cell.out")
+    # le o arquivo cell.in
+    n, m, d, k, i = reader.readInput() 
+    # monta o automato celular
     automaton = Automaton(size=n, limit=m, distance=d, initialValues=i)
+    # inicia a engine
     engine = Engine(executions=k, automaton=automaton)
-
+    # executa os steps
     engine.run()
-    print(automaton)
+    # escreve o retorno em cell.out
+    reader.writeOutput(str(automaton))
 
 main()
         
